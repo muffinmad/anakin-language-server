e17c1e23186ad5c11dd4b1322556f9feeb513fef
muffinmad <andreyk.mad@gmail.com>
2020-05-05 01:33:53 +0300

    v1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='anakin-language-server',
-    version='1.2.8',
+    version='1.3',
     author='Andrii Kolomoiets',
     author_email='andreyk.mad@gmail.com',
     description='Yet another Jedi Python language server',
