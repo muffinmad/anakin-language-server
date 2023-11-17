f0101d45092260d6acb7bd81a6d11eaaff3f2f06
muffinmad <andreyk.mad@gmail.com>
2023-11-17 10:29:24 +0200

    Format all files

--- anakinls/version.py
+++ anakinls/version.py
@@ -1,1 +1,8 @@
 __version__ = '1.19'
+
+__copyright__ = """
+Copyright (C) 2020 Andrii Kolomoiets
+This is free software; see the source for copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+"""
